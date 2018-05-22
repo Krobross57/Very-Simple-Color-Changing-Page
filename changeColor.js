59bdfb9c32ac482dc750b145a9c47ec45a610b47
antho <krobross_57@hotmail.fr>
2018-05-22 22:07:32 +0200

    Ajout d'une temporisation à 5000 ms pour le changement de couleur

--- changeColor.js
+++ changeColor.js
@@ -5,9 +5,9 @@ function ChangeTextColor() {
     this.changeColor = function() {
         
         this.text.style.color = "blue";
-    }
+    };
     
-    
+    setTimeout(this.changeColor, 5000);
 
 }
 
